67f182da11d99b4cee5b3f746e809149abc4ca88
Code Cadet <pauloroc.pt@gmail.com>
2017-03-24 01:51:49 +0000

    Added Secure passaword archive

--- src/main/java/org/academiadecodigo/roothless/Main.java
+++ src/main/java/org/academiadecodigo/roothless/Main.java
@@ -8,7 +8,9 @@ import org.academiadecodigo.roothless.model.dao.hibernate.HibernateUserDao;
 import org.academiadecodigo.roothless.persistence.TransactionManager;
 import org.academiadecodigo.roothless.persistence.hibernate.HibernateSessionManager;
 import org.academiadecodigo.roothless.persistence.hibernate.HibernateTransactionManager;
+import org.academiadecodigo.roothless.persistence.jdbc.ConnectionManager;
 import org.academiadecodigo.roothless.service.ServiceRegistry;
+import org.academiadecodigo.roothless.service.jdbc.JdbcUserService;
 import org.academiadecodigo.roothless.service.user.HibernateUserService;
 import org.academiadecodigo.roothless.service.user.UserService;
 import org.academiadecodigo.roothless.service.user.UserServiceImpl;
@@ -20,27 +22,37 @@ import org.academiadecodigo.roothless.service.user.UserServiceImpl;
 public class Main extends Application {
 
 
+    private ConnectionManager connectionManager;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void init() {
 
+        System.out.println(Thread.currentThread().getName());
 
+        connectionManager = new ConnectionManager();
+
+        // Instantiate a user service
         UserDao userDao = new HibernateUserDao();
         TransactionManager transactionManager = new HibernateTransactionManager();
         UserService userService = new UserServiceImpl(userDao, transactionManager);
-
-
-
-        HibernateUserService hibernateUserService = new HibernateUserService();
-
-        ServiceRegistry.getInstanceService().addService(hibernateUserService);
-
-        Navigation.getInstance().setStage(primaryStage);
-        Navigation.getInstance().loadScreen("registration_view");
     }
+
+        @Override
+        public void start (Stage primaryStage)throws Exception {
+
+            primaryStage.setTitle("Five Star Reservations");
+
+            HibernateUserService hibernateUserService = new HibernateUserService();
+
+            ServiceRegistry.getInstanceService().addService(hibernateUserService);
+
+            Navigation.getInstance().setStage(primaryStage);
+            Navigation.getInstance().loadScreen("registration_view");
+
+            primaryStage.show();
+        }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-
 }
--- src/main/java/org/academiadecodigo/roothless/controller/RegistrationController.java
+++ src/main/java/org/academiadecodigo/roothless/controller/RegistrationController.java
@@ -2,12 +2,39 @@ package org.academiadecodigo.roothless.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import org.academiadecodigo.roothless.Navigation;
+import org.academiadecodigo.roothless.model.User;
+import org.academiadecodigo.roothless.service.ServiceRegistry;
+import org.academiadecodigo.roothless.service.user.UserService;
+import org.academiadecodigo.roothless.utils.Security;
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by Paulo Rocha on 21-03-2017.
  */
-public class RegistrationController {
+public class RegistrationController implements Initializable {
+
+    private static final String NAME = "login";
+
+//    public void setService(UserService service) {
+//        this.service = service;
+//    }
+
+    private boolean login = true;
+
+    public static String getName() {
+        return NAME;
+    }
+
+    @FXML
+    Pane pane;
 
     //register and login buttons
     @FXML private Button register_button;
@@ -28,11 +55,112 @@ public class RegistrationController {
     @FXML private TextField username_register_field;
     @FXML private TextField creditcard_register_field;
     @FXML private TextField expiration_register_field;
+    @FXML private Label errorLabel;
 
 
-    @FXML
-    void login(ActionEvent event) {}
 
-    @FXML
-    void register(ActionEvent event) {}
-}
+
+        UserService userService;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+            userService = (UserService) ServiceRegistry.getInstanceService().getService(UserService.class.getSimpleName());
+
+
+        assert username_login_field != null : "fx:id=\"username_login_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert password_login_field != null : "fx:id=\"password_login_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert login_button != null : "fx:id=\"login_button\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert email_register_field != null : "fx:id=\"email_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert register_button != null : "fx:id=\"register_button\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert mobile_register_field != null : "fx:id=\"mobile_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert nif_register_field != null : "fx:id=\"nif_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert password_verify_register_field != null : "fx:id=\"password_verify_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert password_register_field != null : "fx:id=\"password_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert username_register_field != null : "fx:id=\"username_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert creditcard_register_field != null : "fx:id=\"creditcard_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert expiration_register_field != null : "fx:id=\"expiration_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert lastname_register_field != null : "fx:id=\"lastname_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+        assert firstname_register_field != null : "fx:id=\"firstname_register_field\" was not injected: check your FXML file 'registration_view.fxml'.";
+    }
+
+
+    private void doLogin() {
+
+        if (username_login_field.getText().isEmpty()) {
+            showConsoleText("username missing");
+            return;
+        }
+
+        if (password_login_field.getText().isEmpty()) {
+            showConsoleText("password missing");
+            return;
+        }
+
+        if (!userService.authenticate(username_login_field.getText(), password_login_field.getText())) {
+            showConsoleText("authentication failed");
+            return;
+        }
+
+        showConsoleText("login accepted");
+        Navigation.getInstance().loadScreen("credits_view.fxml");
+
+    }
+
+
+
+    private void doRegister() {
+
+        if (username_register_field.getText().isEmpty()) {
+            showConsoleText("username missing");
+            return;
+        }
+
+        if (password_register_field.getText().isEmpty()) {
+            showConsoleText("password missing");
+            return;
+        }
+
+        if (email_register_field.getText().isEmpty()) {
+            showConsoleText("email missing");
+            return;
+        }
+
+        if (userService.findByName(username_register_field.getText()) != null) {
+            showConsoleText("username taken");
+            return;
+        }
+
+        userService.addUser(new User(username_register_field.getText(), Security.getHash(password_register_field .getText()), email_register_field.getText(), "client"));
+
+        if (userService.findByName(username_register_field.getText()) == null) {
+            showConsoleText("registration failed");
+            return;
+        }
+
+        showConsoleText("registration successful");
+
+    }
+
+    private void showConsoleText(String text) {
+
+        errorLabel.setText("console.log(\"" + text + "\");");
+        errorLabel.setVisible(true);
+
+    }
+
+    public void login_button_pressed(ActionEvent event) {
+
+        if (login) {
+            doLogin();
+        } else {
+            doRegister();
+        }
+
+    }
+
+    public void register_button_pressed(MouseEvent event) {
+        doRegister();
+        }
+
+    }
